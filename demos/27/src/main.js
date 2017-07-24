efead141918b57863fbb9140e5fa13e44e797bf5
Allie Grampa <agrampa@yahoo.com>
2017-07-24 16:25:49 -0700

    basic h1 for form  returned

--- demos/27/src/main.js
+++ demos/27/src/main.js
@@ -3,7 +3,17 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
-  
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Form Demo</h1>
+      </div>
+    )
+  }
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
