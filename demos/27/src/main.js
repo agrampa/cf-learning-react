c153a75cb130a82376f895fb535e2cfd47acfed4
Allie Grampa <agrampa@yahoo.com>
2017-07-24 16:54:34 -0700

    added basic form component

--- demos/27/src/main.js
+++ demos/27/src/main.js
@@ -2,6 +2,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// create a form container component every time you creat a form
+// the component will have its own state and will manage the state of the form's inputs
+// a form container is a component that holds the state for a form's inputs
+class PokemonForm extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <form>
+
+      </form>
+    )
+  }
+}
+
+
+
 class App extends React.Component {
   constructor(props){
     super(props);
