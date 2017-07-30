5899d7fbc2aedd4487d0f6e5749414f5c6a8c844
Allie Grampa <agrampa@yahoo.com>
2017-07-30 11:56:15 -0700

    added expense input form

---
+++ demos/29/src/component/expense-create-form/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class ExpenseCreateForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      price: 0,
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return(
+      <form className='expense-create-form'
+        onSubmit={this.handleSubmit}
+        // value and onChange are required for EVERY input
+        <input name='title' type='text' value={this.state.title} onChange={this.handleChange} />
+        <input name='price' type='text' value={this.state.price} onChange={this.handleChange} />
+
+        <button type='submit'> + Add Expense </button>
+      </form>
+    )
+  }
+}
