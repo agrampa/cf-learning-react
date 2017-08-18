8bbdd708ec61e8ebc23e3c3430db3345d2976bd3
Allie Grampa <agrampa@yahoo.com>
2017-08-18 16:37:29 -0700

    adding board actions

--- demos/31/redux-demo/reducer/category.js
+++ demos/31/redux-demo/reducer/category.js
@@ -1,1 +1,21 @@
 'use strict'
+
+initialState = []
+reducer = (state=initialState, action) {
+  let {type, payload} = action
+
+  switch(type) {
+    // three operations for the state
+
+    // take board as payload and push into array
+    case 'CATEGORY_CREATE':
+
+    // take board as payload and update it in the array
+    case 'CATEGORY_UPDATE':
+
+    // take board as payload and delete it from the array
+    case 'CATEGORY_DELETE':
+    default:
+      return state
+  }
+}
