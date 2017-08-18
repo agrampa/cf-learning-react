703bbfeff87c88ea1ba248680371c1a9ece16318
Allie Grampa <agrampa@yahoo.com>
2017-08-18 16:52:30 -0700

    added reset functionality to reducer

--- demos/31/redux-demo/reducer/category.js
+++ demos/31/redux-demo/reducer/category.js
@@ -4,7 +4,7 @@ initialState = []
 
 // categories will have an id and a title
 
-reducer = (state=initialState, action) {
+reducer = (state=initialState, action) => {
   let {type, payload} = action
 
   switch(type) {
@@ -17,10 +17,17 @@ reducer = (state=initialState, action) {
     // take category as payload and update it in the array, using category id
     case 'CATEGORY_UPDATE':
       return state.map(category => category.id == payload.id ? payload : category)
+
     // take category as payload and delete it from the array
     case 'CATEGORY_DELETE':
       return state.filter(category => category.id != payload.id)
+
+    // override to reset back to original state
+    case 'CATEGORY_RESET'
+      return initialState
     default:
       return state
   }
 }
+
+// if reducer is called without one of the cases, it will return the state/initialState
