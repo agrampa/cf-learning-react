8ec65e06341f04763547b5e7205b39068ee32e7f
Allie Grampa <agrampa@yahoo.com>
2017-08-18 16:41:42 -0700

    added create, update, and delete category functionality

--- demos/31/redux-demo/reducer/category.js
+++ demos/31/redux-demo/reducer/category.js
@@ -1,20 +1,25 @@
 'use strict'
 
 initialState = []
+
+// categories will have an id and a title
+
 reducer = (state=initialState, action) {
   let {type, payload} = action
 
   switch(type) {
     // three operations for the state
 
-    // take board as payload and push into array
+    // take category as payload and push into array
     case 'CATEGORY_CREATE':
+      return [...state, payload]
 
-    // take board as payload and update it in the array
+    // take category as payload and update it in the array, using category id
     case 'CATEGORY_UPDATE':
-
-    // take board as payload and delete it from the array
+      return state.map(category => category.id == payload.id ? payload : category)
+    // take category as payload and delete it from the array
     case 'CATEGORY_DELETE':
+      return state.filter(category => category.id != payload.id)
     default:
       return state
   }
