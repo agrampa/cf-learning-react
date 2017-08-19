c948c323b8f4b2c0a713724d7116d2599365d1fd
Allie Grampa <agrampa@yahoo.com>
2017-08-18 17:53:41 -0700

    added subscribe

--- demos/31/redux-demo/index.js
+++ demos/31/redux-demo/index.js
@@ -2,12 +2,20 @@ import {createStore} from 'redux'
 import categoryReducer from '././reducer/category.js'
 import {categoryCreate, categoryUpdate, categoryDelete, categoryReset} from './action/category-actions.js'
 
+// store is the interface to the state
 
 const store = createStore(categoryReducer)
+
+// subscribe can be called multiple times with other callbacks
+// every time an action is dispatched and state has updated, subscribe will be called
+store.subscribe(() => {
+  console.log('__STATE__', store.getState())
+})
 
 // store.getState() will return the initial state of the store as defined in the reducer
 
 // dispatch an action
+// dispatch takes the state of the store, and replaces it with whatever results from dispatching/calling the action
 store.dispatch(categoryCreate({title: 'cool beans'}))
 
 // now store.getState() will return the new state with the title, and added uuid and timestamp
