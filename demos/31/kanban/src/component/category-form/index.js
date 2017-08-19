6307e7202fd5a93d077e37739c0da5b2f010ed86
Allie Grampa <agrampa@yahoo.com>
2017-08-19 16:20:03 -0700

    added content to categoryForm component

--- demos/31/kanban/src/component/category-form/index.js
+++ demos/31/kanban/src/component/category-form/index.js
@@ -5,7 +5,8 @@ class categoryForm extends React.Component{
     super(props)
 
     this.state = {
-
+      // "this" is optional if it comes after super(props)
+      title: this.props.category ? this.props.category.title : ''
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -13,16 +14,23 @@ class categoryForm extends React.Component{
   }
 
   handleChange(e) {
-
+    this.setState({title: e.target.value})
   }
 
   handleSubmit(e) {
     e.preventDefault()
+    this.props.onComplete(this.state)
   }
 
   render() {
     return(
-      
+      <form className='category-form' onSubmit={this.handleSubmit}>
+        <input name='title' type='text' placeholder='title' value={this.state.input} onChange={this.handleChange} />
+        <button type='submit'> {this.props.buttonText} </button>
+      </form>
     )
   }
 }
+
+// don't need to connect to state from the store
+export default categoryForm
