6f4a54cfd482fbefeced96b2b5f5f1f060a62b77
Allie Grampa <agrampa@yahoo.com>
2017-08-19 15:38:24 -0700

    added basic map functions with example

--- demos/31/kanban/src/component/dashboard-container/index.js
+++ demos/31/kanban/src/component/dashboard-container/index.js
@@ -11,4 +11,32 @@ class DashboardContainer extends React.Component {
   }
 }
 
-export default DashboardContainer
+// returns an object and anything added to the object will become a prop
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch, getState) => {
+  return {
+
+  }
+}
+
+// instead of just exporting the component, now export the component after calling connect with mapStateToProps and mapDispatchToProps, it will return a function that the componenet can be passed through
+// now anything that was passed through with mapStateToProps and mapDispatchToProps can be accessed through props
+export default connect(mapStateToProps, mapDispatchToProps)(default DashboardContainer)
+
+
+
+
+
+// for example:
+const mapStateToProps = state => {
+  return {
+    foo: 'hi'
+  }
+}
+
+then add {this.props.foo} within JSX and it will show the string 'hi'
