a5a07a0f146dcb0202698c092cb258ec0a8b71b6
Allie Grampa <agrampa@yahoo.com>
2017-08-18 18:35:51 -0700

    added provider to App and subscribe functionality within componentDidMount of App

--- demos/31/kanban/src/component/app/index.js
+++ demos/31/kanban/src/component/app/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import {Provider} from 'react-redux'
+// Provider registers the store so, later on, you can connect the store to the components
 import {BrowserRouter, Route} from 'react-dom'
 
+import createAppStore from '../../lib/store.js'
+
+const store = createAppStore()
+
 class App extends React.Component {
+  componentDidMount(){
+    store.subscribe(() => {
+      console.log('__STATE__', store.getState());
+    })
+  }
+
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <h1>Hello from App</h1>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <h1>Hello from App</h1>
+          </BrowserRouter>
+        </Provider>
       </div>
     )
   }
