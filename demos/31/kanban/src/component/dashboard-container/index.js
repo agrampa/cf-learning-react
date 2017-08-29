c32d5b2e9c5a0e311a76f4b2a6c02c2e42f65eb2
Allie Grampa <agrampa@yahoo.com>
2017-08-28 20:54:29 -0700

    added files for 32

--- demos/31/kanban/src/component/dashboard-container/index.js
+++ demos/31/kanban/src/component/dashboard-container/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-import {categoryCreate, categoryUpdate, categoryDelete} from '../../action/ctegory-actions.js'
+import {categoryCreate, categoryUpdate, categoryDelete} from '../../action/category-actions.js'
 
 import CategoryForm from '../category-form'
 
